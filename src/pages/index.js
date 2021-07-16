c338d78bf9da927c3a960d315b1113c8de9d04e5
sowmiyaEE <sowmiya.skm@gmail.com>
2021-07-16 17:54:45 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/Head';
+import Link from 'next/Link';
+import Layout from "../component/Layout";
+import fs from 'fs';
+
+const Home=({ slugs }) => (
+     return(
+<Layout>
+<div>
+    slugs:
+   {slugs.map(slug => {
+ 
+  <div key={slug}>  
+    <Link href={slug}>
+       <a>{slug}</a>
+    </Link>
+  </div>
+  );
+})}
+</div>
+</Layout>
+);
+
+ export const getStaticProps = async() =>
+{
+const files=fs.readdirSync("courses");
+return{
+  {props : {
+     slugs : files.map(filename => filename.replace(".md",""))
+    }
+
+}
+}
+export default Home;
