62d7b420b5524e45f2168cb78ff07b7af0397688
sowmiyaEE <sowmiya.skm@gmail.com>
2021-07-20 18:38:36 +0530

    Delete Layout.js

--- component/Layout.js
+++
@@ -1,6 +0,0 @@
-import Head from "next/Head";
-const Layout=() => (
-return(<Head>
-<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-</Head>););
-export default Layout;
